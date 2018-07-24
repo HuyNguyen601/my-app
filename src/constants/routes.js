1bca3e8fce3fc18ce7d24c92a96591425b152366
HuyNguyen601 <giahuy601@yahoo.com>
2018-07-23 23:44:37 -0700

    Sign In Modal

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,4 @@
 export const SIGN_UP = '/signup';
-export const LANDING = '/';
-export const HOME = '/home';
+export const HOME = '/';
 export const ACCOUNT = '/account';
 export const PASSWORD_FORGET = '/pw-forget';
